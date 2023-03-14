f076c029ced6f20f2596aabdf6011ad32f70b189
Carlos Emilio Cabral <Carlos Emilio Cabral@DESKTOP-UBTP0PR>
2023-03-14 15:31:22 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.vb
@@ -0,0 +1,63 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.btMostrar = New System.Windows.Forms.Button()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(22, 135)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(747, 303)
+        Me.DataGridView1.TabIndex = 0
+        '
+        'btMostrar
+        '
+        Me.btMostrar.Location = New System.Drawing.Point(90, 12)
+        Me.btMostrar.Name = "btMostrar"
+        Me.btMostrar.Size = New System.Drawing.Size(179, 76)
+        Me.btMostrar.TabIndex = 1
+        Me.btMostrar.Text = "Mostrar"
+        Me.btMostrar.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btMostrar)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents btMostrar As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,23 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btMostrar.Click
+        ' Declarar y asignar valores al arreglo multidimensional
+        Dim ventas(,) As Integer = {{100, 200, 300}, {400, 500, 600}, {700, 800, 900}}
+
+        ' Crear una tabla para mostrar en el DataGridView
+        Dim tabla As New DataTable()
+
+        ' Agregar columnas a la tabla
+        tabla.Columns.Add("Producto")
+        tabla.Columns.Add("Mes 1")
+        tabla.Columns.Add("Mes 2")
+        tabla.Columns.Add("Mes 3")
+
+        ' Agregar filas a la tabla con los datos del arreglo
+        tabla.Rows.Add("Producto 1", ventas(0, 0), ventas(0, 1), ventas(0, 2))
+        tabla.Rows.Add("Producto 2", ventas(1, 0), ventas(1, 1), ventas(1, 2))
+        tabla.Rows.Add("Producto 3", ventas(2, 0), ventas(2, 1), ventas(2, 2))
+
+        ' Mostrar la tabla en el DataGridView
+        DataGridView1.DataSource = tabla
+    End Sub
+End Class
